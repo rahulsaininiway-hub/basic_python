e7a887786e573bb3553b61ce24efdd792b914b36
Rahul saini <rahulsaininiway@gmail.com>
2026-03-20 08:30:22 +0530

    Add files via upload

--- for_table.py
+++ for_table.py
@@ -1,4 +1,6 @@
-a=int(input("enter the value"))
-a=a+1
-for i in range(i,a):
-    print(i)
+a=int(input("enter the fast value"))
+b=int(input("enter the last value"))
+for n in range(a, b+1):
+    print()
+    for i in range(1,11):
+        print(n*i, end=" " )
